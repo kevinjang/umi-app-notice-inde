896e4691bd5c1313748ff5026643185ab7915bc1
kevinjang <kevinjang@163.com>
2020-07-30 14:03:15 +0800

    结果有误，再次整理

--- .umirc.js
+++ .umirc.js
@@ -7,6 +7,7 @@ export default {
     compact: true
   },
   dva: {
+    immer: true,
     hmr: true
   },
   dynamicImport: {
--- src/components/GlobalHeader/NoticeIconView.jsx
+++ src/components/GlobalHeader/NoticeIconView.jsx
@@ -1,18 +1,36 @@
 import HeaderDropdown from '../HeaderDropdown/'
 import NoticeIcon from '../NoticeIcon'
+import React, { useEffect } from 'react'
 
 import { connect } from 'umi'
 // console.log('connect:', connect)
-const GlobalHeaderDropdown = ({ fetchingNotices, ...restProps }) => {
-  // console.log('GlobalHeaderDropdown fetchingNotices:', fetchingNotices)
-  return <HeaderDropdown trigger={['click']} {...restProps} >
-    <NoticeIcon>
-      <NoticeIcon.Tab key={"noticeicon.tab1"}>
-        Tab1
-            </NoticeIcon.Tab>
-    </NoticeIcon>
-  </HeaderDropdown>
+
+class GlobalHeaderDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    if (dispatch) {
+      dispatch({
+        type: 'global/fetchNotices'
+      })
+    }
+  }
+
+  render() {
+    const { fetchingNotices } = this.props;
+    return <HeaderDropdown trigger={['click']} {...this.props} >
+      <NoticeIcon loading={fetchingNotices}>
+        <NoticeIcon.Tab key={"noticeicon.tab1"}>
+          Tab1
+        </NoticeIcon.Tab>
+      </NoticeIcon>
+    </HeaderDropdown>
+  }
 }
+
 export default connect(({ global, loading }) => {
   console.log('loading:', loading);
   console.log('niv global:', global)
